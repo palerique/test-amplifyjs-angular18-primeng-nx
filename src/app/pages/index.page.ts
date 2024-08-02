8d17310f6e4fe37f833a26d2d83a3bcb0dab1181
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2024-08-02 17:33:41 -0300

    feat: integrate PrimeFlex for layout enhancements
    Added PrimeFlex for improved grid system and layout management.
    Updated styles.scss to include necessary PrimeFlex and PrimeNG imports.
    Modified pnpm-lock.yaml and package.json to include PrimeFlex dependency.
    Updated index.page.ts to properly utilize the layout changes.
    
    Provides a more efficient and flexible layout structure.

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -14,14 +14,14 @@ import {AppSidebarComponent} from '../components/sidebar/app.sidebar.component';
       <div class="layout-sidebar">
         <app-sidebar></app-sidebar>
       </div>
-      <!--      <div class="layout-main-container">-->
-      <!--        <div class="layout-main">-->
-      <router-outlet></router-outlet>
-      <!--        </div>-->
-      <!--        <app-footer></app-footer>-->
-      <!--      </div>-->
+      <div class="layout-main-container">
+        <div class="layout-main">
+          <router-outlet></router-outlet>
+        </div>
+        <!--        <app-footer></app-footer>-->
+      </div>
       <!--      <app-config></app-config>-->
-      <!--      <div class="layout-mask"></div>-->
+      <div class="layout-mask"></div>
     </div>
   `,
   styles: [],
