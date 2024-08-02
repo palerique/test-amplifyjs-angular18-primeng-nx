96f5bd761dd1361b7425cbd74315cdb912a1a93b
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2024-08-02 11:30:17 -0300

    feat: add google code style

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import * as eslintConfigGoogle from "eslint-config-google";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -9,4 +10,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
+  eslintConfigGoogle,
 ];
