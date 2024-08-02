766dd76ffe6cf3ae5902110072cbad899d535e2e
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2024-08-02 11:12:35 -0300

    feat(app.config): add async animations support
    Added `provideAnimationsAsync` to the app configuration to enable
    asynchronous animations in the application. This change improves the
    performance and responsiveness of animations.
    
    No breaking changes introduced.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideFileRouter(),
     provideHttpClient(withFetch()),
     provideClientHydration(),
